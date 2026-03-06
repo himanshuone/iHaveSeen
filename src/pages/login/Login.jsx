6db8be1ec46cd47c735a366f79defa8ef9527549
Himanshu Kumar <79431391+himanshuone@users.noreply.github.com>
2026-03-07 00:09:23 +0530

    Login Completed

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -1,6 +1,7 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 function Login() {
+    const [account, setaccount] = useState(true);
     return (
         <>
             <div className='w-screen h-screen flex font-playfair'>
@@ -36,14 +37,41 @@ function Login() {
                     <div className='bg-gray-100 h-fit border border-gray-300 rounded-xl w-full max-w-105 min-w-105 p-10 items-center justify-center shadow-black'>
 
                         <div className='bg-gray-200 p-1 flex rounded-xl space-x-1 w-full justify-evenly'> <div className='bg-white items-center w-full p-2 flex justify-center rounded-xl'>Sign In</div> <div className='bg-white items-center w-full p-2 flex justify-center rounded-xl'>Create Account</div></div>
-                        <div className='text-2xl mt-8  font-semibold'>Welcome back</div>
-                        <div className='mt-2' > <p className='font-sans text-gray-500 text-sm'>Sign in to your account</p></div>
-                        <div className='mt-4 font-sans'>
-                            <form onSubmit={() => console.log("Hello")}>
-                                <label htmlFor="Email">Email</label>
-                                <div className='border border-gray-500 rounded p-2 focus-within:border-pink-500 bg-gray-200'><input type="email" className='w-full h-full outline-0' />
+                        <div>
+                            <div className='text-2xl mt-8  font-semibold'>Welcome back</div>
+                            <div className='mt-2' > <p className='font-sans text-gray-500 text-sm'>Sign in to your account</p></div>
+                            <div className='mt-4 font-sans'>
+                                <form onSubmit={() => console.log("Hello")}>
+                                    <div>
+                                        <label htmlFor="Email">Email</label>
+                                        <div className='border border-gray-300 rounded p-2 focus-within:border-pink-500 bg-gray-200'><input type="email" className='w-full h-full outline-0' />
+                                        </div>
+                                    </div>
+                                    <div className='mt-4'>
+                                        <label htmlFor="Password">Password</label>
+                                        <div className='flex items-center w-full gap-3'><div className='border border-gray-300 rounded p-2 focus-within:border-pink-500 bg-gray-200 w-[90%]'><input type="email" className='w-full h-full outline-0' />
+
+                                        </div>
+                                            <div className='h-full ' onClick={() => console.log("Password Visible")}>👀</div>
+                                        </div></div>
+                                    <div className='text-sm font-light mt-2 w-full flex justify-end'>Forget Password?</div>
+                                    <div className=' flex w-full border-0 bg-gray-300 p-2.5 rounded-xl items-center justify-center mt-2'>Sign In</div>
+
+                                </form>
+                                <div className='flex items-center text-xs mt-4'>
+                                    <div className='h-px flex-1 bg-gray-400'></div>
+                                    <span className='px-2 text-gray-400'>or continue with</span>
+                                    <div className='h-px flex-1 bg-gray-400'></div>
                                 </div>
-                            </form>
+                                <div className='flex w-full gap-2 mt-4'>
+                                    <div className='border-0 flex flex-1 bg-gray-200 p-2 rounded-xl items-center justify-center'>Google</div>
+                                    <div className='border-0 flex flex-1 bg-gray-200 p-2 rounded-xl justify-center items-center'>Github</div>
+                                </div>
+                                <div>
+                                    <div className='flex text-sm justify-center items-center mt-4 gap-1'><div className='font-light text-gray-400'> Don't have an account?</div> <div className='font'>Create one</div></div>
+                                </div>
+
+                            </div>
                         </div>
 
 
