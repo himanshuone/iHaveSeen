f525061969c8b34803072a0ac44c6df12352819b
Himanshu Kumar <79431391+himanshuone@users.noreply.github.com>
2026-03-26 11:45:17 +0530

    error handling

--- src/routes/AppRoute.jsx
+++ src/routes/AppRoute.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import NotValidRoute from '../pages/NotValidRoute'
-import MainLayout from '../Layouts/MainLayout'
+import MainLayout from '../layouts/MainLayout'
 import { Route, Routes } from 'react-router-dom'
 import Sidebar from '../components/Sidebar/Sidebar'
 import DashBoard from '../Pages/DashBoard'
@@ -17,10 +17,10 @@ function AppRoute() {
           <Route element={<MainLayout />}>
               <Route path='/' element={<DashBoard/>} />
               <Route path='login' element={<Login/>} />     
-                <Route path='addmovie' element={<AddMovie/>} />
+                <Route path='/addmovie' element={<AddMovie/>} />
         
-              <Route path='MovieList' element={<MovieList/>} />
-              <Route path='MovieDetail' element={<MovieDetail/>} />
+              <Route path='/movieList' element={<MovieList/>} />
+              <Route path='/movieDetail' element={<MovieDetail/>} />
           </Route>
           <Route path='*' element={<NotValidRoute />} />
       </Routes>
